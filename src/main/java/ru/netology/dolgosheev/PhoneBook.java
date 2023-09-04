214554bfb18e8518cfea78d12c93058d1d90e612
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-09-04 22:20:09 +0300

    add implemented

--- src/main/java/ru/netology/dolgosheev/PhoneBook.java
+++ src/main/java/ru/netology/dolgosheev/PhoneBook.java
@@ -1,8 +1,16 @@
 package ru.netology.dolgosheev;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
+    private Map<String, Integer> phoneBook = new HashMap<>();
+
     public int add(String name, int number) {
-        return 0;
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.put(name, number);
+        }
+        return phoneBook.size();
     }
 }
